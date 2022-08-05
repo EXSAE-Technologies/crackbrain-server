787d9a6a775602868971e7971eb3425114290f3a
fshangala <fshangala@gmail.com>
2022-08-05 23:12:22 +0200

    latest

--- crackbrain/settings.py
+++ crackbrain/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-xyp(gq5-7k1^mvh1i$dl2(61m#5m^v)it0y)qd%#_i1y*z=z#h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
